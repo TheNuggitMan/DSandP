3ff43923707cda08b5df3f5214ba303264e1e6b7
TheNuggitMan <64538216+TheNuggitMan@users.noreply.github.com>
2020-07-10 17:09:37 +0100

    added calender

--- js/master.js
+++ js/master.js
@@ -1,3 +1,7 @@
 function openModal() {
   document.getElementById("lightbox").style.display = "block";
 }
+
+var picker = new Lightpick({ field: document.getElementById('datepicker'),
+singleDate: false
+});
