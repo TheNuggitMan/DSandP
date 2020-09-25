2470100e93456ac32228e50d005595ed71a944fa
TheNuggitMan <thenuggitman@gmail.com>
2020-09-25 16:46:55 +0100

    added js auto-scroll

--- js/master.js
+++ js/master.js
@@ -1,6 +1,13 @@
-function openModal() {
-  document.getElementById("lightbox").style.display = "block";
-}
+document.getElementById('landing').scrollIntoView({
+  behavior: 'smooth'
+});
+
+
+
+//
+// function openModal() {
+//   document.getElementById("lightbox").style.display = "block";
+// }
 
 var picker = new Lightpick({ field: document.getElementById('datepicker'),
 singleDate: false
